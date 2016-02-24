22ec272bedc4358fbea547b5cb5de90319682dd7
Bud Byrd <bud.byrd@gmail.com>
2016-02-24 13:00:06 -0600

    Initial commit.

---
+++ grails-app/controllers/com/budjb/IndexController.groovy
@@ -0,0 +1,14 @@
+package com.budjb
+
+import org.codehaus.groovy.grails.commons.GrailsApplication
+
+/**
+ * Created by budbyrd on 2/24/16.
+ */
+class IndexController {
+    GrailsApplication grailsApplication
+
+    def index() {
+        render grailsApplication.config.vdo.rules
+    }
+}
